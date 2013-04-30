9e0d600fa25d15ac67ba3cd2e7dd1d7ff8819f4f
Brent Goldstein <brentlanegold@gmail.com>
2013-04-30 00:36:19 -0400

    colored ratings

--- app.js
+++ app.js
@@ -3,33 +3,50 @@ var theApp = angular.module('theApp',[]);
 
 theApp.controller('mainController', ['$scope', 'movieServices', function mainController($scope, movieServices){
 
-    $scope.selectedMovie = "none";
-    $scope.movies=["Ironman", "Darkcity", "Angelheart", "Spartacus"];
+    //   $scope.selectedMovie = "none";
     $scope.movies = movieServices.getMovieList();
 
-    var callbacks = $scope.callbacks = {};
+    var callbacks = $scope.movieCallbacks = {};
     callbacks.setValue = function(movie){
-        $scope.selectedMovie = movie;
-       var m_info = movieServices.getMovieInfo(movie);
-       $scope.synopsis = m_info.synopsis;
-       $scope.quote = m_info.quote;
+        movieServices.selected = $scope.selectedMovie = movie;
+        var m_info = movieServices.getMovieInfo(movie);
+        $scope.synopsis = m_info.synopsis;
+        $scope.quote = m_info.quote;
+        $scope.ratingLevel = m_info.rating;
+        $scope.ratingColor = $scope.getMovieColor(m_info.rating);
     }
 
-    $scope.movieCallbacks = function(){
-          return $scope.callbacks;
+    callbacks.getValue = function(){
+        return movieServices.selected;
     }
 
- /*
-    $scope.$watch('selectedMovie', function(){
-       prompt("movie changed")
-    });
-   */
+    $scope.getMovieColor = function(rating){
+        var color;
+        switch(rating){
+            case "PG13":
+                color = "green";
+                break;
+            case "R":
+                color = "orange"
+                break;
+            case "Unrated":
+                color = "red"
+                break
+            default:
+                color = "blue"
+                break;
+        }
+        return color;
+    }
+
+    /*
+     $scope.$watch('selectedMovie', function(){
+     prompt("movie changed")
+     });
+     */
 
 }]);
 
-theApp.controller('customController1',[ function customController1($scope){
-
-}]);
 
 theApp.directive('customButtons', function(){
     return{
@@ -38,26 +55,30 @@ theApp.directive('customButtons', function(){
         scope: {
             //replace: true,
             options: '=',
-            selected: '=',
+            //           selected: '=',
+            hoverText: '=',
             handlers: '=',
             addlClasses: '@'
-        },
-        link: function($scope, elm, attrs){
-            $scope.activate = function(option){
-                var callbacks = $scope.handlers;
-                console.log("callbacks: " + callbacks.setValue)
-                callbacks.setValue(option);
-                //var setter = callbacks().setValue;
-                //console.log("setter: " + setter)
-                //setter("WUBBA");
 
-                //$scope.selected = option;
+        },
+        link: function(scope, elm, attrs){
+            scope.activate = function(option){
+                var callbacks = scope.handlers;
+                callbacks.setValue(option);
             };
+
+            scope.currentlyActive = function(){
+                var callbacks = scope.handlers;
+                return callbacks.getValue();
+            }
+
+            //           elm.css('title', $cope.hoverText);
+
         },
         //templateUrl: "templateFilePath",
         template: "<button class='cust-button {{addlClasses}}' " +
             "ng-repeat='option in options' " +
-            "ng-class='{active: option == selected}'" +
+            "ng-class='{active: option == currentlyActive()}'" +
             "ng-click='activate(option)'>" +
             "{{option}}" +
             "</button>"
@@ -65,35 +86,52 @@ theApp.directive('customButtons', function(){
     };
 });
 
+theApp.directive( 'moviecoloring', ['movieServices', function(movieServices) {
+    return {
+        restrict: 'AC',
+        link: function (scope, element, attrs) {
+            scope.$watch(movieServices.selected, function(value){
+                console.log("at watch " + value);
+
+            });
+        }
+    }
+}]);
+
+
 theApp.factory('movieServices',["$rootScope", function($rootScope){
 
     var movieDB = {
         "Dark City": {
             synopsis: "A man struggles with memories of his past, including a wife he cannot remember, in a nightmarish world with no sun and run by beings with telekinetic powers who seek the souls of humans."
             ,quote:"Dr. Schreber: Remember John, never talk to strangers!"
+            ,rating:"R"
         },
         "Ironman":{
             synopsis:"When wealthy industrialist Tony Stark is forced to build an armored suit after a life-threatening incident, he ultimately decides to use its technology to fight against evil."
             ,quote:"Tony Stark: I'd like to show you firsthand."
+            ,rating:"PG13"
 
         },
         "Angel Heart": {
             synopsis:"Harry Angel has a new case, to find a man called Johnny Favourite. Except things aren't quite that simple, and Johnny doesn't want to be found. Let's just say that, amongst the period detail and beautiful scenery, it all gets really, really nasty."
             ,quote:"Louis Cyphre: The flesh is weak, Johnny. Only the soul is immortal."
+            ,rating:"R"
 
         },
         "Spartacus: 'Blood and Sand'":{
             synopsis:"Before Spartacus struck down his first opponent in the arena, there were many gladiators who passed though the gates onto the sand.'"
             ,quote:"Aurelia: He was not a perfect man; but he was mine! He was mine!"
+            ,rating:"Unrated"
         }
     };
+    var selected = "None";
     var movieList = [];
     for( m in movieDB ){
         if(movieDB.hasOwnProperty(m)){
             movieList.push(m);
         }
     }
-
 
     var that={};
 
@@ -113,7 +151,7 @@ theApp.factory('movieServices',["$rootScope", function($rootScope){
 
 
 
-    angular.module('theApp.values', []).
+angular.module('theApp.values', []).
     value('version', '0.1').
     value('releaseLevel', 'alpha');
 
