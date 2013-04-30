3110448a12ddce556f717ddddbdc7542f9be9e4b
Brent Goldstein <brentlanegold@gmail.com>
2013-04-30 15:47:34 -0400

    separate out directives

--- app.js
+++ app.js
@@ -30,61 +30,6 @@ theApp.controller('mainController', ['$scope', 'movieServices', function mainCon
 }]);
 
 
-theApp.directive('customButtons', function(){
-    return{
-        //replace: true,
-        restrict: 'E',
-        scope: {
-            options: '=',
-//           selected: '=',
-            handlers: '=',
-            addlClasses: '@'
-
-        },
-        controller: function($scope){
-            $scope.activate = function(option){
-                var callbacks = $scope.handlers;
-                callbacks.setActive(option);
-            };
-
-            $scope.currentlyActive = function(){
-                var callbacks = $scope.handlers;
-                return callbacks.getActive();
-            }
-
-            $scope.getTitle = function(option){
-                console.log("option: " + option)
-                var callbacks = $scope.handlers;
-                return callbacks.titleText(option);
-            }
-
-        },
-        // see the file, button_partial.html for a better read, but
-        // need to use the inline template since there's no web server
-        template: "<button class='cust-button {{addlClasses}}' " +
-            "ng-repeat='option in options' " +
-            "ng-class='{active: option == currentlyActive()}'" +
-            "title='{{getTitle(option)}}'" +
-            "ng-click='activate(option)'>" +
-            "{{option}}" +
-            "</button>"
-
-    };
-});
-
-theApp.directive( '$rootscope', ['movieServices', function($rootscope) {
-    return {
-        restrict: 'AC',
-        link: function (scope, element, attrs) {
-            $rootscope.$watch('selected', function(value){
-                console.log("at watch " + value);
-
-            });
-        }
-    }
-}]);
-
-
 theApp.factory('movieServices',["$rootScope", function($rootScope){
 
     var movieDB = {
